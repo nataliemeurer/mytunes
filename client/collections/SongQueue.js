a3166b706b968b3192ae0d707e3b254827c12cc9
kmeurer <kevin.a.meurer@gmail.com>
2014-09-24 16:18:52 -0700

    stuff

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,34 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function(song){
+      if(this.length === 1 ) {
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('ended', function(){
+      this.remove(this.at(0));
+      if( this.length ){
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('dequeue', function(song){
+      this.remove(song);
+    }, this);
+  },
+
+
+  enqueue: function(song){
+    this.add(song);
+    if(this.length ===1 ) {
+      this.playFirst();
+    }
+  },
+
+  playFirst: function(){
+    this.at(0).play();
   }
 
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -15,6 +15,11 @@ var AppModel = Backbone.Model.extend({
 
     params.library.on('play', function(song){
       this.set('currentSong', song);
+
+    }, this);
+
+    params.library.on('enqueue', function(song){
+      this.get('songQueue').enqueue(song);
     }, this);
   }
 
